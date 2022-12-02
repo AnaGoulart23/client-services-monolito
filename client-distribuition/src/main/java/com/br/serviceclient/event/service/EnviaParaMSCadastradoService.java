ba44cbc52742db4ea495868551a9db194139a288
anamarciagoulart <ana.neto@klab.com.br>
2022-12-02 14:01:57 -0300

    adiciona requisicao com httpclient

--- client-distribuition/src/main/java/com/br/serviceclient/event/service/EnviaParaMSCadastradoService.java
+++ client-distribuition/src/main/java/com/br/serviceclient/event/service/EnviaParaMSCadastradoService.java
@@ -2,11 +2,14 @@ package com.br.serviceclient.event.service;
 
 
 import com.br.serviceclient.response.ClienteResponse;
-import coresearch.cvurl.io.model.Response;
-import coresearch.cvurl.io.request.CVurl;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.google.gson.Gson;
 
-import javax.xml.transform.Result;
-import java.util.Map;
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
 
 public class EnviaParaMSCadastradoService implements Runnable {
     ClienteResponse clienteResponse;
@@ -17,14 +20,30 @@ public class EnviaParaMSCadastradoService implements Runnable {
 
     @Override
     public void run() {
-        CVurl cVurl = new CVurl();
-        Response<String> msOne = cVurl.post("http://localhost:8160/service-receiver-one/")
-                .body(clienteResponse)
-                .asString()
-                .orElseThrow();
-        System.out.println(msOne.getBody());
+        try {
+            String requestBody = prepareRequest();
+            HttpClient httpClient = HttpClient.newHttpClient();
+            HttpRequest request = HttpRequest
+                    .newBuilder()
+                    .uri(URI.create(clienteResponse.getUrlNotification()))
+                    .POST(HttpRequest.BodyPublishers.ofString(requestBody))
+                    // .header("Content-Type", "application/json")
+                    .build();
 
+            HttpResponse<String> response = httpClient.send(request, HttpResponse.BodyHandlers.ofString());
+            System.out.println(response.body());
+            System.out.println(response.body().toString());
+            System.out.println(response.statusCode());
+        } catch (Throwable e) {
+            //erros 500 sao capturados aqui
+            // erros 400 ignorados
+            e.printStackTrace();
+        }
     }
 
+    private String prepareRequest() throws JsonProcessingException {
+        Gson gson = new Gson();
+        return  gson.toJson(clienteResponse);
+    }
 
 }
